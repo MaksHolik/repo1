09affa7b239a1d49b0dd64b57af8b8af926f7b88
MaksHolik <maksholik@gmail.com>
2022-01-02 23:56:28 +0100

    commit4

--- server01.js
+++ server01.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku! mała zmiana w pliku</h1>")
 })
 
+app.get("/", function (req, res) {
+    res.send("{imie: \"Maksymilian\", nazwisko: \"Holik\", klasa: \"3i2\", grupa: \"a\"}")
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
